3b0af424d7278b165e4e0abe8d1800ea635fe69f
apclarity <andreaspeterc@gmail.com>
2025-06-14 10:26:20 +0700

    refactor: minor code cleanup

--- src/components/molecules/UserAccordion.tsx
+++ src/components/molecules/UserAccordion.tsx
@@ -30,14 +30,14 @@ const UserAccordion: React.FC<UserAccordionProps> = ({ users, selectedUser, repo
                     >
                         <span>{user.login}</span>
                         <span className={`transition-transform duration-300 ${isOpen ? "rotate-180" : ""}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 16 16" fill="currentColor">
-                  <path
-                    fillRule="evenodd"
-                    d="M11.78 9.78a.75.75 0 0 1-1.06 0L8 7.06 5.28 9.78a.75.75 0 0 1-1.06-1.06l3.25-3.25a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 16 16" fill="currentColor">
+                            <path
+                                fillRule="evenodd"
+                                d="M11.78 9.78a.75.75 0 0 1-1.06 0L8 7.06 5.28 9.78a.75.75 0 0 1-1.06-1.06l3.25-3.25a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06Z"
+                                clipRule="evenodd"
+                            />
+                            </svg>
+                        </span>
                     </button>
                     {isOpen && (
                     <div className="p-3 space-y-2 w-full">
